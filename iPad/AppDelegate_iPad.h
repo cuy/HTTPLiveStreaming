11a0e1681014cdeabb762f23e00e1246c6d938d7
Charles <cuy@g2ix.net>
2010-12-16 13:18:41 +0800

    Initial commit

---
+++ iPad/AppDelegate_iPad.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPad.h
+//  HTTPLiveStreaming
+//
+//  Created by Charles Joseph Uy on 12/16/10.
+//  Copyright 2010 G2iX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPad : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPhone.h
+//  HTTPLiveStreaming
+//
+//  Created by Charles Joseph Uy on 12/16/10.
+//  Copyright 2010 G2iX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPhone : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
